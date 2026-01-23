3585ec456034f9ddefa87184cfb019f2245b92fc
YoussefOssamaa <y.ossama45@gmail.com>
2026-01-23 03:05:44 +0000

    Add The getPluginByID Route

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MarketplacePage from './screens/PluginMarketplace';
-
+import { PricingPlansPage } from './pages/pricingPlans/PricingPlansPage';
+import { PluginReviewPage } from './pages/plugins-review/PluginReviewPage';
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/marketplace" element={<MarketplacePage />} />
         <Route path="/" element={<MarketplacePage />} />
+        <Route path="/marketplace" element={<MarketplacePage />} />
+        <Route path="/pricingPlans" element={<PricingPlansPage />} />
+        <Route path="/pluginReview" element={<PluginReviewPage />} />
       </Routes>
     </Router>
   );
